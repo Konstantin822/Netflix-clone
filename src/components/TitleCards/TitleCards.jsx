a92792935f484218d10a2c72276a15c352d055e1
Konstantin <bladflag8@gmail.com>
2024-11-05 12:08:19 +0200

    c

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({ title, category }) => {
-    // ------- Scroll cards list -------
+
     const [apiData, setApiData] = useState([])
     const cardsRef = useRef()
 
@@ -27,17 +28,17 @@ const TitleCards = ({ title, category }) => {
 
         cardsRef.current.addEventListener('wheel', handleWheel)
     }, [])
-    // ----------------
+
 
     return (
         <div className='title-cards'>
             <h2>{title ? title : "Popular on Netflix"}</h2>
             <div className='title-cards__card-list' ref={cardsRef}>
                 {apiData.map((card, index) => {
-                    return <div className='card' key={index}>
+                    return <Link to={`/player/${card.id}`} className='card' key={index}>
                         <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} />
                         <p>{card.original_title}</p>
-                    </div>
+                    </Link>
                 })}
             </div>
         </div>
